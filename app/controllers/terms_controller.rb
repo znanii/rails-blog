c7f0c2f1262070e8a45dc6ec2bad9f1047c67421
zhanii <naborznanii@gmail.com>
2024-03-28 20:28:54 -0700

    add terms (index)

---
+++ app/controllers/terms_controller.rb
@@ -0,0 +1,6 @@
+class TermsController < ApplicationController
+
+def index
+  puts "Our terms are just like everywhere else"
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
  get 'home/index'
-
+ get '/terms' => 'terms#index'
+ #resource :terms, only: [:index], path_names: {:index => ''}
  resource :contacts, only: [:new, :create], path_names: { :new => ''}
  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
