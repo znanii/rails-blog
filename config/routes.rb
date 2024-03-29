137f54ff17f813a90970058d1884a2a1ab2eee2b
zhanii <naborznanii@gmail.com>
2024-03-28 20:40:58 -0700

    '/about'
    1)rails g controller about
    2)def index - end(about_controller)
    3)routes.rb (get 'about/index')
    4)views - index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
  get 'home/index'
  get '/terms' => 'terms#index'
- #resource :terms, only: [:index], path_names: {:index => ''}
+get '/about' => 'about#index'
+ 
  resource :contacts, only: [:new, :create], path_names: { :new => ''}
  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
