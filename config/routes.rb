a602555ca5143747a5c326998319acbc685c4548
zhanii <naborznanii@gmail.com>
2024-03-29 22:50:25 -0700

    one to many - comments - create model and route
    article:references

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 #get '/about' => 'about#index'
  
  resource :contacts, only: [:new, :create], path_names: { :new => ''}
- resources :articles
+ resources :articles do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_27_003546) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_30_053428) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -25,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_27_003546) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
