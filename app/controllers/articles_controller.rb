1480446e8c2864e77d029a05ff57aa1df8d65b32
zhanii <naborznanii@gmail.com>
2024-04-04 21:33:44 -0700

    authenticate user only to create article/comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :only => [:create]
 
   def index
     @articles = Article.order('created_at DESC') 
