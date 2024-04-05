b19860861fd77d6c6231bc2123ba9e02ac8836b0
zhanii <naborznanii@gmail.com>
2024-04-04 20:49:52 -0700

    add username column to user table

---
+++ db/migrate/20240405033637_add_username.rb
@@ -0,0 +1,6 @@
+class AddUsername < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :username, :string
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_183830) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_05_034705) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
@@ -42,8 +42,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_183830) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "articles"
