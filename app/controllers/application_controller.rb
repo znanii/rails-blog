1ac69d9d22a743e6dadca4fb1023a8c76e3a8ec4
zhanii <naborznanii@gmail.com>
2024-04-04 21:05:18 -0700

    permit username for sign-up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
 	#protect_from_forgery with: :exception я сделала
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+ private
+
+ def configure_permitted_parameters
+ # devise_parameter_sanitizer.for(:sign_up) << :username
+ devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+ end
+
 end
