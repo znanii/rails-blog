3d454c84e3b6518796dfb7704ca587e4a52a25dc
zhanii <naborznanii@gmail.com>
2024-04-07 23:00:51 -0700

    art_w_com factory in Articles factory (creates 3 comments)

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :article do
     title { "Article title" }
     text {"Article text" }
+
+
+#create factory with the name article_with_comments
+# to create an article with several comments
+    factory :article_with_comments do
+      after :create do |article, evaluator|
+        #create list with 3 comments
+        create_list :comment, 3, article: article
+      end  
+    end
   end
 end
